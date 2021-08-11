4e05da51457088c192f3c43a363582966087b148
Nolan-Rewega <88743689+Nolan-Rewega@users.noreply.github.com>
2021-08-10 18:24:26 -0600

    Deleted unused code directory

--- src/unused code/load_frame.h
+++
@@ -1,9 +0,0 @@
-#ifndef LOAD_FRAME_H
-#define LOAD_FRAME_H
-
-#include<iostream>
-using namespace std;
-
-bool load_frame(const char* filename, int* width, int* height, unsigned char** data);
-
-#endif
